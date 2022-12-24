9ccf904ec8469c4fa4a44a804770fe9578c82d01
Arek <arek.stachyra41@gmail.com>
2022-12-24 14:28:29 +0100

    add state to hold general information and handle it on inputs

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,58 @@ import WorkExperiencePreview from './components/WorkExperiencePreview';
 import EducationPreview from './components/EducationPreview';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			general: {
+				name: '',
+				surname: '',
+				role: '',
+				phone: '',
+				email: '',
+				description: '',
+			},
+			// workExperience: [
+			// 	{
+			// 		id: crypto.randomUUID(),
+			// 		companyName: '',
+			// 		position: '',
+			// 		startDate: '',
+			// 		endDate: '',
+			// 		description: '',
+			// 	},
+			// ],
+		};
+
+		this.handleGeneralChange = this.handleGeneralChange.bind(this);
+	}
+
+	handleGeneralChange(e) {
+		const { target } = e;
+		const value =
+			target.type === 'checkbox' ? target.checked : target.value;
+		const { name } = target;
+
+		this.setState({
+			// eslint-disable-next-line react/no-access-state-in-setstate
+			general: { ...this.state.general, [name]: value },
+		});
+	}
+
 	render() {
 		return (
-			<main className="mx-auto flex h-screen max-w-7xl space-x-10 py-10 px-8">
-				<section className="flex-1 space-y-8 text-sm">
-					<General />
+			<main className="mx-auto flex max-w-7xl flex-col space-y-10 py-10 px-8 lg:flex-row lg:space-x-10">
+				<section className="space-y-8 text-sm lg:flex-1">
+					<General
+						general={this.state.general}
+						handleGeneralChange={this.handleGeneralChange}
+					/>
 					<WorkExperience />
 					<Education />
 				</section>
-				<section className="flex-[2] border border-black p-4">
-					<GeneneralPreview />
+				<section className="lg:flex-[2] lg:self-start">
+					<GeneneralPreview general={this.state.general} />
 					<WorkExperiencePreview />
 					<EducationPreview />
 				</section>
