70fc249d6e36f52e4a299a103868dc7b07f47579
Arek <arek.stachyra41@gmail.com>
2022-12-24 14:30:10 +0100

    add preview for general information

--- src/components/GeneralPreview.jsx
+++ src/components/GeneralPreview.jsx
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 
 class GeneralPreview extends Component {
 	render() {
-		return <p>General Preview</p>;
+		const { name, surname, role, phone, email, description } =
+			this.props.general;
+		return (
+			<div className="bg-slate-400">
+				<span className="block">{name}</span>
+				<span className="block">{surname}</span>
+				<span className="block">{role}</span>
+				<span className="block">{phone}</span>
+				<span className="block">{email}</span>
+				<span className="block">{description}</span>
+			</div>
+		);
 	}
 }
 
