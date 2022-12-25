993ea740f5a5d2e84ae6e1fc646b967970f7312e
Arek <arek.stachyra41@gmail.com>
2022-12-25 20:57:11 +0100

    move education form into seperate component

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,76 +1,29 @@
 import React, { Component } from 'react';
+import EducationForm from './forms/EducationForm';
 
 class Education extends Component {
 	render() {
+		const { education, handleEducationChange, handleAddForm } = this.props;
+
 		return (
 			<div>
 				<h3 className="mb-2 text-lg font-bold">Education</h3>
-				<form className="space-y-2">
-					<div>
-						<label htmlFor="major" className="mb-1 block">
-							Major
-						</label>
-						<input
-							type="text"
-							name="major"
-							id="major"
-							className="block w-full rounded-md border-gray-300 px-2 py-1"
+				<div className="space-y-8">
+					{education.map((educationState) => (
+						<EducationForm
+							key={educationState.id}
+							education={educationState}
+							handleEducationChange={handleEducationChange}
 						/>
-					</div>
-					<div>
-						<label htmlFor="university" className="mb-1 block ">
-							University
-						</label>
-						<input
-							type="text"
-							name="university"
-							id="university"
-							className="block w-full rounded-md border-gray-300 px-2 py-1"
-						/>
-					</div>
-					<div>
-						<label
-							htmlFor="educationStartDate"
-							className="mb-1 block "
-						>
-							Start Date
-						</label>
-						<input
-							type="date"
-							name="educationStartDate"
-							id="educationStartDate"
-							className="block w-full rounded-md border-gray-300 px-2 py-1"
-						/>
-					</div>
-					<div>
-						<label
-							htmlFor="educationEndDate"
-							className="mb-1 block "
-						>
-							End Date
-						</label>
-						<input
-							type="date"
-							name="educationEndDate"
-							id="educationEndDate"
-							className="block w-full rounded-md border-gray-300 px-2 py-1"
-						/>
-					</div>
-					<div>
-						<label
-							htmlFor="educationDescription"
-							className="mb-1 block "
-						>
-							Description
-						</label>
-						<textarea
-							type="text"
-							name="educationDescription"
-							id="educationDescription"
-							className="block w-full rounded-md border-gray-300 px-2 py-1"
-						/>
-					</div>
-				</form>
+					))}
+				</div>
+				<button
+					type="button"
+					onClick={() => handleAddForm('education')}
+					className="mt-4 rounded-md bg-indigo-500 px-4 py-2 text-white transition-colors hover:bg-indigo-700"
+				>
+					Add
+				</button>
 			</div>
 		);
 	}
