2ef3bd8860de0b0a7fe32edc4b1eddca070a1e3d
Arek <arek.stachyra41@gmail.com>
2022-12-25 20:56:49 +0100

    move files

--- src/components/EducationPreview.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class EducationPreview extends Component {
-	render() {
-		return <p>Education Preview</p>;
-	}
-}
-
-export default EducationPreview;
--- src/components/WorkExperiencePreview.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class WorkExperiencePreview extends Component {
-	render() {
-		return <p>Work Experience Preview</p>;
-	}
-}
-
-export default WorkExperiencePreview;
