59e3d1d999e26cfa0e2d34753cf58a9db33d0298
Arek <arek.stachyra41@gmail.com>
2022-12-26 14:09:16 +0100

    adjust preview list styles

--- src/components/EducationPreviewList.jsx
+++ src/components/EducationPreviewList.jsx
@@ -5,14 +5,17 @@ class EducationPreviewList extends Component {
 	render() {
 		const { education } = this.props;
 		return (
-			<>
+			<div className="mt-6 space-y-4">
+				<h3 className="mb-3 text-base font-bold text-indigo-800">
+					Education
+				</h3>
 				{education.map((educationState) => (
 					<EducationPreview
 						key={educationState.id}
 						education={educationState}
 					/>
 				))}
-			</>
+			</div>
 		);
 	}
 }
--- src/components/WorkExperiencePreviewList.jsx
+++ src/components/WorkExperiencePreviewList.jsx
@@ -5,14 +5,17 @@ class WorkExperiencePreviewList extends Component {
 	render() {
 		const { workExperience } = this.props;
 		return (
-			<>
+			<div className="mt-6 space-y-4">
+				<h3 className="mb-3 text-base font-bold text-indigo-800">
+					Work Experience
+				</h3>
 				{workExperience.map((workExperienceState) => (
 					<WorkPreview
 						key={workExperienceState.id}
 						workExperience={workExperienceState}
 					/>
 				))}
-			</>
+			</div>
 		);
 	}
 }
